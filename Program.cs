0368201364d99137efeffe59661e92674a1103ad
emmaraindropsrni7 <raindropsrni7+emma@gmail.com>
2022-10-25 16:16:06 +0100

    print messages in green

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("--> I'm printing in green!");
 Console.WriteLine("Hello, World!");
